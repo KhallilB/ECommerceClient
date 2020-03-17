8a4d87a3a8f77211c7581f8d57810dae6461d51f
KhallilB <khallil.bailey@gmail.com>
2020-03-16 18:20:56 -0700

    [add] Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';
+
+class Navbar extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Navbar Works!</h1>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
