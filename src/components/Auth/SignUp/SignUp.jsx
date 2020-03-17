2d7b48eb804045df38777f3772a6aa40f51de909
KhallilB <khallil.bailey@gmail.com>
2020-03-17 10:37:35 -0700

    [add] signup componenet

---
+++ src/components/Auth/SignUp/SignUp.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './SignUp.css';
+
+class SignUp extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Signup works!</h1>
+            </div>
+        )
+    }
+}
+
+export default SignUp;
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import {
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import LogIn from './components/Auth/LogIn/LogIn';
+import SignUp from './components/Auth/SignUp/SignUp';
 
 const Routes = () => {
   return (
@@ -14,6 +15,11 @@ const Routes = () => {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route
+          path='/auth/signup'
+          exact
+          component={SignUp}
+        />
         <Route path='/auth/login' exact component={LogIn} />
       </Switch>
     </Router>
