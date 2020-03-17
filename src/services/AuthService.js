9e7023176a7b1c31c52ea3749db0b856dedef5f9
KhallilB <khallil.bailey@gmail.com>
2020-03-17 13:56:17 -0700

    [fix]  auth service api call

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -6,38 +6,42 @@ class AuthService {
     this.login = this.login.bind(this);
   }
 
-  async signup(firstName, lastName, email, password) {
-    await axios
-      .post(`${API}/auth/signup `, {
-        firstName,
-        lastName,
-        email,
-        password
-      })
-      .then(res => {
-        console.log(res);
-        return res;
-      })
-      .catch(err => {
-        console.log(err);
-        return err;
-      });
+  signup(firstName, lastName, email, password) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(`${API}/auth/signup`, {
+          firstName,
+          lastName,
+          email,
+          password
+        })
+        .then(res => {
+          console.log(res);
+          resolve(res);
+        })
+        .catch(err => {
+          console.log(err);
+          reject(err);
+        });
+    });
   }
 
   async login(email, password) {
-    await axios
-      .post(`${API}/auth/login`, {
-        email,
-        password
-      })
-      .then(res => {
-        console.log(res);
-        return res;
-      })
-      .catch(err => {
-        console.log(err);
-        return err;
-      });
+    return new Promise((resolve, reject) => {
+      axios
+        .post(`${API}/auth/login`, {
+          email,
+          password
+        })
+        .then(res => {
+          console.log(res);
+          resolve(res);
+        })
+        .catch(err => {
+          console.log(err);
+          reject(err);
+        });
+    });
   }
 }
 
